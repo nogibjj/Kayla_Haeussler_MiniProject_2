13b5e8a12b7e0233ddaf565c8769ba69b0f622d5
Kayla Haeusssler <kaylahaeusssler@Kaylas-MBP-2.CocoaCinnamonLKWD.DurhamNC.TLD>
2024-09-15 18:04:45 -0400

    Functions in main.py updated, switched out dataset

--- README.md
+++ README.md
@@ -1,20 +1,20 @@
-# Kayla_Haeussler_MiniProject_1
+# Kayla_Haeussler_MiniProject_2
 
 [![CI](https://github.com/nogibjj/Kayla_Haeussler_MiniProject_1/actions/workflows/hello.yml/badge.svg)](https://github.com/nogibjj/Kayla_Haeussler_MiniProject_1/actions/workflows/hello.yml)
 
 
-IDS 706: Mini Project 1
+IDS 706: Mini Project 2
+Pandas Descriptive Statistics Script
 Kayla Haeussler
 
 The purpose of this assignment was to create a  Python template to be used for future projects with best practices in place for testing, development and deployment.
 
 
-# Included Features
-- .devcontainer
-- Dockerfile
-- Makefile: containing make install, make test, make lint
-- CI/CD Pipeline
-- Requirements.txt
+# Requirements
+- Python script using Pandas for descriptive statistics
+- Read a dataset (CSV or Excel)
+- Generate summary statistics (mean, median, standard deviation)
+- Create at least one visualization
 
 The main function (defined in main.py) is the add function, which takes two variables, num1 and num2, and returns their sum. Tests for this function are defined in test.py
 
--- main.py
+++ main.py
@@ -3,16 +3,44 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Loadind csv data set
-df = pd.read_csv('ClassicHit.csv')
 
-# Generating sumamry statistics
-print(df)
+def readcsv(filename):
+    return pd.read_csv(filename)
 
-# Creating visualization
-plt.figure(figsize=(10, 6))
-sns.histplot(df['Duration'].dropna(), bins=30, kde=True)
-plt.title('Histogram of Your Song Duration')
-plt.xlabel('Duration')
-plt.ylabel('Frequency')
-plt.show()
+def get_summary_stats(dataset, col_of_intrst):
+    summ_stats = dataset[col_of_intrst].describe()
+    print("Mean final exam score  is " + str(round(summ_stats["mean"], 3)) + " minutes")
+    print("Median final exam score is "+ str(round(summ_stats["50%"], 3)) + " minutes")
+    print("Standard Deviation of final exam score is " + str(round(summ_stats["std"], 3)) + " minutes")
+
+    return summ_stats
+
+def create_histogram(df, col_of_intrst): 
+    plt.figure(figsize=(10, 6))
+    sns.histplot(df[col_of_intrst].dropna(), bins=30, kde=True)
+    plt.title('Histogram of Final Exam Scores')
+    plt.xlabel("Exam Score (out of 100)")
+    plt.ylabel('Frequency')
+    plt.show()
+
+def create_scatter(data, x_col, y_col):
+    data.plot.scatter(x = x_col, y= y_col)
+    plt.title(x_col +" vs. "+ y_col)
+    plt.xlabel(x_col)
+    plt.ylabel(y_col)
+    plt.show()
+
+
+if __name__ == "__main__":
+    # read the csv
+    df = readcsv("StudentPerformanceFactors.csv")
+
+
+
+    # get summary stats: mean, median, sd
+    summary_stats = get_summary_stats(df, "Exam_Score")
+    print(summary_stats)
+
+    # create visualizations
+    create_histogram(df, "Exam_Score")
+    create_scatter(df, "Hours_Studied", "Exam_Score")
