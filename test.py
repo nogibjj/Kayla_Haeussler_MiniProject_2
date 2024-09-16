cb434296df6a883a2b3901858826becd0e6421cd
Kayla Haeusssler <kaylahaeusssler@Kaylas-MBP-2.lan>
2024-09-15 23:08:17 -0400

    Edits for CI/CD

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ from main import (
 import pandas as pd
 
 def test_get_summary_stats():
-    summary_stats= (pd.read_csv("StudentPerformanceFactors.csv")).describe()
+    summary_stats= get_summary_stats(pd.read_csv("StudentPerformanceFactors.csv"), "Exam_Score")
     assert summary_stats["mean"] == 67.235659
     assert summary_stats["50%"] == 67
     assert summary_stats["std"] == 3.890456
