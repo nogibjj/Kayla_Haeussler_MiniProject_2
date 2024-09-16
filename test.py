0c7612455a83f30ce4cea96e481bcfe5db8b4220
Kayla Haeusssler <kaylahaeusssler@Kaylas-MBP-2.lan>
2024-09-15 23:20:53 -0400

    updated for CI/CD

--- test.py
+++ test.py
@@ -5,13 +5,14 @@ from main import (
 )
 
 import pandas as pd
+import pytest
+from pytest import approx
 
 def test_get_summary_stats():
     summary_stats= get_summary_stats(pd.read_csv("StudentPerformanceFactors.csv"), "Exam_Score")
-    assert summary_stats["mean"] == 67.235659
+    assert summary_stats["mean"] == approx(67.235659, rel=1e-2)
     assert summary_stats["50%"] == 67
-    assert summary_stats["std"] == 3.890456
-
+    assert summary_stats["std"] == approx(3.890456, rel=1e-2)
 
 
 def test_create_histogram():
